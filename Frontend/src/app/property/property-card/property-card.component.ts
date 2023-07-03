32c9dc48aaf6bb34505730e2433a0f8759613810
sssourav-m <sourav.maji.54321@gmail.com>
2023-07-03 13:55:15 +0530

    Till pipe

--- Frontend/src/app/property/property-card/property-card.component.ts
+++ Frontend/src/app/property/property-card/property-card.component.ts
@@ -1,4 +1,5 @@
-import { Component } from "@angular/core";
+import { Component, Input } from "@angular/core";
+import { IProperty } from "../IProperty.interface";
 
 
 @Component({
@@ -11,11 +12,5 @@ import { Component } from "@angular/core";
 
 )
 export class PropertyCardComponent {
-
-  Property: any = {
-    "Id":1,
-    "Name":"Birla House",
-    "Type":"House",
-    "Price":12000
-  }
+  @Input() property: IProperty;
 }
